e8131ae20fc1823b7ae009e2502af7efdd579681
toMan <tomasman1979@gmail.com>
2019-09-12 21:15:45 +0200

    adding comments to the code

--- one_line.py
+++ one_line.py
@@ -1,29 +1,45 @@
 import tkinter as tk
 
-class Odlamovac(tk.Tk):
-    def __init__(self):
+class OneLine(tk.Tk):
+    """
+    Simple app to remove line breaks
+    and make just single line of text from input.
+    """
+    def __init__(self): # Tkinter main window
         tk.Tk.__init__(self)
-        self.title('Odlamovač')
+        # window title
+        self.title('One Line')
+        # field for input text
         self.entry = tk.Text(self, height=10, width=100)
         self.entry.pack()
-        self.nahradnik = tk.Label(self, text="Znak pro nahrazení konce řádku:", width=100)
-        self.nahradnik.pack()
-        self.nahradnik = tk.Entry(self, width=20)
-        self.nahradnik.pack()
-        self.button = tk.Button(self, text="ODLOMIT", command=self.odlom)
+        # input of character or string to replace end of the line
+        self.replacement = tk.Label(self, text="Replacement for the end of the line:", width=100)
+        self.replacement.pack()
+        self.replacement = tk.Entry(self, width=20)
+        self.replacement.pack()
+        # exec button
+        self.button = tk.Button(self, text="NO END", command=self.noEnd)
         self.button.pack()
-        self.vystup = tk.Text(self, height=5, width=100)
-        self.vystup.pack()
-        self.button = tk.Button(self, text="KONEC", command=self.destroy)
-        self.button.pack()
+        # output field
+        self.result = tk.Text(self, height=5, width=100)
+        self.result.pack()
+        # button to quit the app
+        self.button = tk.Button(self, text="exit", padx=20, command=self.destroy)
+        self.button.pack(anchor="e")
         
 
-    def odlom(self):
-        nahrada = self.nahradnik.get()
-        vstup = self.entry.get('1.0','end-1c')
-        odlomeno = vstup.replace("\n", nahrada)
-        self.vystup.delete('1.0','end-1c')
-        self.vystup.insert('1.0', odlomeno)
+    def noEnd(self): # function to remove line breaks
+        # obtaining replacement string
+        replaceCharacter = self.replacement.get()
+        # reading the input with avoiding the last linebreak
+        inputText = self.entry.get('1.0','end-1c')
+        # replacing line breaks
+        outputText = inputText.replace("\n", replaceCharacter)
+        # emptying output field before writing the result
+        self.result.delete('1.0','end-1c')
+        # writing output
+        self.result.insert('1.0', outputText)
 
-app = Odlamovac()
+#initializing the main loop
+app = OneLine()
 app.mainloop()
