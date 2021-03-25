fb6430df8843b46449ccf497adbecd0b31410343
toMan <tomasman1979@gmail.com>
2021-03-25 11:14:59 +0100

    added rename JPG script and update readme

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Colection of small Python scripts.
 
+## rename_by_exif.py
+Rename images in selected directory by [EXIF](https://exif.readthedocs.io/en/latest/index.html) datetime_origin value. Skip files with missing exif or no JPG extension. Python 3.6+, directory selection dialogue in Tkinter.
+
 ## one-line.py
 Simple linebreak remover. Uses Tkinter as GUI, nothing fancy. You can choose string to replace linebreaks and decide if removing multiple spaces.
 
---
+++ rename_by_exif.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# script to rename images in selected folder according to EXIF datetime_origin
+
+from exif import Image # external library - pip3 install exif
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+def set_directory() -> str:
+    '''tkinter dialogue to select directory for processing
+
+    retur directory path as a string
+    '''
+    path_find = tk.Tk()
+    path_find.withdraw()
+    path = tk.filedialog.askdirectory() + "/"
+    return path
+
+def exif_date_time(file_path: str) -> str:
+    '''retrieve EXIF informations from file
+
+    return string in format: YYYY-mm-dd_HHMMSS
+    return False if not image or missing exif
+    '''
+    try:
+        with open(file_path, 'rb') as image_file:
+            my_image = Image(image_file)
+            exD = my_image.datetime_original
+            exif_datetime = exD[:4]+"-"+exD[5:7]+"-"+exD[8:10]+"_"+exD[11:13]+exD[14:16]+exD[-2:]
+            return exif_datetime
+    except Exception:
+        return False
+
+def exif_rename(folder_path: str, jpg: str, datetime: str):
+    '''rename one JPG file'''
+    src = folder_path + jpg # old filename path
+    dst = folder_path + datetime + ".jpg"
+    os.rename(src, dst)
+
+
+
+if __name__ == "__main__":
+    isJpg = (".jpg", ".jpeg") #alowed file extensions
+    processed = 0
+    passed = 0
+    duplicity = 0
+    files_list_clear = [] # only JPG's
+    folder_path = set_directory() # call for folder selection dialogue
+    files_list = os.listdir(folder_path) # list files in directory
+    # clear list - only JPG files
+    for file in files_list: 
+        try:
+            extension = file[file.rindex("."):]
+            if extension.lower() in isJpg:
+                files_list_clear.append(file)
+        except ValueError:
+            continue
+    # rename files if EXIF datetime find
+    for jpg in files_list_clear:
+        datetime = exif_date_time(folder_path + jpg)
+        # skip no exif files
+        if datetime == False: 
+            passed += 1
+            pass
+        # skip exif duplicity images (edited versions etc...)
+        elif os.path.isfile(folder_path + datetime + ".jpg") == True:
+            duplicity += 1
+            pass
+        # if all clear, rename file
+        else: 
+            exif_rename(folder_path, jpg, datetime)
+            processed += 1
+           
+    # final report
+    if processed > 0:
+        print("Images renamed: " + str(processed))
+        print("Images skiped: " + str(passed))
+        if duplicity > 0:
+            print("There was " + str(duplicity) + " skiped images alredy renamed or the same exif datetime_origin")
+    else:
+        print("No images out of " + str(len(files_list)) + " processed - already exists or missing exif.")
