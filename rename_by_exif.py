9c2f7d669bc930abc25e5c3d3152d963f027e643
mantomas <tomasman1979@gmail.com>
2021-07-01 22:08:08 +0200

    change datetime processing to strptime/strftime

--- rename_by_exif.py
+++ rename_by_exif.py
@@ -3,10 +3,10 @@
 
 import os
 import sys
-from exif import Image  # external library - pip3 install exif
 import tkinter as tk
+from datetime import datetime
+from exif import Image  # external library - pip3 install exif
 from tkinter import filedialog
-
 
 
 def set_directory() -> str:
@@ -33,19 +33,9 @@ def exif_date_time(file_path: str) -> str:
     try:
         with open(file_path, "rb") as image_file:
             my_image = Image(image_file)
-            exD = my_image.datetime_original
-            exif_datetime = (
-                exD[:4]
-                + "-"
-                + exD[5:7]
-                + "-"
-                + exD[8:10]
-                + "_"
-                + exD[11:13]
-                + exD[14:16]
-                + exD[-2:]
-            )
-            return exif_datetime
+            exD = datetime.strptime(my_image.datetime_original, "%Y:%m:%d %H:%M:%S")
+            exif_date_time = datetime.strftime(exD, "%Y-%m-%d_%H%M%S")
+            return exif_date_time
     except Exception:
         return False
 
@@ -58,40 +48,38 @@ def exif_rename(folder_path: str, jpg: str, datetime: str):
 
 
 if __name__ == "__main__":
-    isJpg = (".jpg", ".jpeg")  # alowed file extensions
+    is_jpg = (".jpg", ".jpeg")  # alowed file extensions
     processed = 0
     passed = 0
     duplicity = 0
     files_list_clear = []  # only JPG's
     folder_path = set_directory()  # call for folder selection dialogue
     files_list = os.listdir(folder_path)  # list files in directory
-    if files_list is False:
-        pass
-    elif len(files_list) == 0:
+    if len(files_list) == 0:
         print("Empty directory")
     else:
         # clear list - only JPG files
         for file in files_list:
             try:
                 extension = file[file.rindex("."):]
-                if extension.lower() in isJpg:
+                if extension.lower() in is_jpg:
                     files_list_clear.append(file)
             except ValueError:
                 continue
         # rename files if EXIF datetime find
         for jpg in files_list_clear:
-            datetime = exif_date_time(os.path.join(folder_path, jpg))
+            date_time = exif_date_time(os.path.join(folder_path, jpg))
             # skip no exif files
-            if datetime is False:
+            if date_time is False:
                 passed += 1
                 pass
             # skip exif duplicity images (edited versions etc...)
-            elif os.path.isfile(os.path.join(folder_path, datetime + ".jpg")) is True:
+            elif os.path.isfile(os.path.join(folder_path, date_time + ".jpg")) is True:
                 duplicity += 1
                 pass
             # if all clear, rename file
             else:
-                exif_rename(folder_path, jpg, datetime)
+                exif_rename(folder_path, jpg, date_time)
                 processed += 1
 
         # final report
